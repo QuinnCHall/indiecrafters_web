a688016edc928c3dc619c1bdfc1038f9e8cf6e2d
Michael Hall <mhall119@gmail.com>
2021-06-20 18:54:13 -0400

    Add first pages, no theme yet

--- membersites/models.py
+++ membersites/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.shortcuts import reverse
+from slugify import slugify
 
 # Create your models here.
 class MemberSite(models.Model):
@@ -8,5 +10,13 @@ class MemberSite(models.Model):
     discord = models.URLField(blank=True, null=True)
     description = models.TextField()
 
+    @property
+    def slug(self):
+        return slugify(self.name)
+
+    def get_absolute_url(self):
+        #return "/sites/%s/%s/" % (self.id, self.slug)
+        return reverse("show_site", kwargs={"site_id": self.id, "site_slug":self.slug})
+    
     def __str__(self):
         return self.name
