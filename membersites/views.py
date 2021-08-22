b393f23cdf338f3b2d9d579a2af683e4fd9ed9f0
Michael Hall <mhall119@gmail.com>
2021-08-22 15:47:59 -0400

    Enable member server submission form

--- membersites/views.py
+++ membersites/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from membersites.models import MemberServer
+from membersites.models import Member, MemberServer, MemberServerForm, ApprovalStatus
 
 # Create your views here.
 def homepage(request):
-    servers = MemberServer.objects.all()
+    servers = MemberServer.objects.filter(status=ApprovalStatus.APPROVED)
+    if request.method == 'POST':
+        form = MemberServerForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            new_server = form.save()
+            return redirect('show_server', server_id=new_server.id, server_slug=new_server.slug)
+    else:
+        form = MemberServerForm()
     context = {
         'servers': servers,
+        'form': form,
     }
     return render(request, 'index.html', context)
 
 def list_servers(request):
-    servers = MemberServer.objects.all()
+    servers = MemberServer.objects.filter(status=ApprovalStatus.APPROVED)
     context = {
         'servers': servers,
     }
